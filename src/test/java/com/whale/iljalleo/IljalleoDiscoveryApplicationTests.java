ebc4fbaa0510f69f2bfe58b225bbac046b11d688
whale912 <whale912@naver.com>
2024-01-21 16:55:43 +0900

    Initial commit

---
+++ src/test/java/com/whale/iljalleo/IljalleoDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whale.iljalleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IljalleoDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
